1bedaea93b6e02ea4126b5f5882acadde53f90e4
nester-dev <nester.dev@gmail.com>
2022-05-09 17:26:09 +0300

    feat: add information about the project

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@ import changeLang from './functions/changeLang.js';
 const Keyboard = {
 	elements: {
 		container: null,
+		header: null,
 		textarea: null,
 		keyboard: null,
+		lang: null,
+		descr: null,
 	},
 
 	properties: {
@@ -24,16 +27,28 @@ const Keyboard = {
 
 	init() {
 		this.elements.container = document.createElement('div');
+		this.elements.header = document.createElement('div');
 		this.elements.textarea = document.createElement('textarea');
 		this.elements.keyboard = document.createElement('div');
+		this.elements.descr = document.createElement('div');
+		this.elements.lang = document.createElement('div');
 		this.elements.container.classList.add('container');
 		this.elements.textarea.classList.add('textarea');
 		this.elements.keyboard.classList.add('keyboard');
+		this.elements.header.classList.add('header');
+		this.elements.descr.classList.add('descr');
+		this.elements.lang.classList.add('lang');
 
+		this.elements.container.appendChild(this.elements.header);
+		this.elements.header.textContent = 'Виртуальная клавиатура';
+		this.elements.descr.textContent = 'Клавиатура создана в ОС Windows';
+		this.elements.lang.textContent = 'для переключения языка используйте левые alt + shift';
 		this.elements.container.appendChild(this.elements.textarea);
 		this.properties.currLang = this.getLocalStorage() || 'en';
 		createKeys(this.elements.keyboard, this.languages[this.properties.currLang]);
 		this.elements.container.appendChild(this.elements.keyboard);
+		this.elements.container.appendChild(this.elements.descr);
+		this.elements.container.appendChild(this.elements.lang);
 		document.body.appendChild(this.elements.container);
 
 		document.querySelectorAll('.keyboard__key').forEach((value) => {
